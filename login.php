9e59df4ec4e1ae16f1d5517fa28f1a975f5a0a9b
Richard Anatsui <richardanatsui@gmail.com>
2020-10-31 22:53:23 +0000

    added login functionality

---
+++ login.php
@@ -0,0 +1,53 @@
+<?php
+
+if(isset($_POST['submit'])){
+    require 'database.php';
+
+    $email= $_POST['email'];
+    $password=$_POST['password'];
+
+    if(empty($email)||empty($password)){
+        
+        header("Location: signIn.php?error=emptyfields");
+        exit();
+    }else{
+        $sql="select * from customer where email = ?";
+        $stmt= mysqli_stmt_init($conn);
+        if(!mysqli_stmt_prepare($stmt,$sql)){
+
+            header("Location: signIn.php?error=sqlerror0");
+            exit();
+        }else{
+            mysqli_stmt_bind_param($stmt, "s", $email);
+            mysqli_stmt_execute($stmt);
+            $result= mysqli_stmt_get_result($stmt);
+            if($row=mysqli_fetch_assoc($result)){
+                $passCheck=password_verify($password, $row['password']);
+                if(passCheck==false){
+
+                    header("Location: signIn.php?error=wrongpass");
+                    exit();
+                }elseif(passCheck==true){
+                    session_start();
+                    $_SESSION['sessionId']=$row['CID'];
+                    $_SESSION['sessionEmail']=$row['email'];
+                    $_SESSION['sessionFname']=$row['fname'];
+                    $_SESSION['sessionLname']=$row['lname'];
+                    header("Location: index.php?success=logged_in");
+                }else{
+                    header("Location: signIn.php?error=wrong1pass");
+                }
+            }else{
+
+                header("Location: signIn.php?error=nouser");
+                exit();
+            }
+        }
+    }
+    
+}else{
+
+    header("Location: index.php?error=accessforbidden");
+    exit();
+}
+?>
--- signIn.php
+++ signIn.php
@@ -47,21 +47,21 @@
                 <img  class= "thumbnail" src="./assets/signup1.jpg" alt="">
             </div>
             <div class="col-sm-6">
-                <form>
+                <form action="login.php" method="post">
                 <div class="form-group">
                     <label for="exampleInputEmail1">Email address</label>
-                    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+                    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" name="email">
                     <!-- <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small> -->
                 </div>
                 <div class="form-group">
                     <label for="exampleInputPassword1">Password</label>
-                    <input type="password" class="form-control" id="exampleInputPassword1">
+                    <input type="password" class="form-control" id="exampleInputPassword1" name="password">
                 </div>
                 <!-- <div class="form-group form-check">
                     <input type="checkbox" class="form-check-input" id="exampleCheck1">
                     <label class="form-check-label" for="exampleCheck1">Check me out</label>
                 </div> -->
-                <button type="submit" class="btn btn-primary">Submit</button>
+                <button type="submit" class="btn btn-primary" name="submit">Submit</button>
                 </form>
 
                 <h5><em>New User?</em> <button type="submit" class="btn btn-secondary" onclick="window.location.href='signUp.php';">Sign Up Here</button></h5>
