eec8569f6fa4ea530dcb0b627ac5471ab0780ac2
Richard Anatsui <richardanatsui@gmail.com>
2020-11-30 03:41:17 +0000

    added query to insert into orders table

--- charge.php
+++ charge.php
@@ -1,5 +1,6 @@
 <?php
 session_start();
+require_once 'database.php';
 require_once('vendor/autoload.php');
 // require_once('vendor/stripe-php/init.php');
 \Stripe\Stripe::setApiKey('sk_test_51HQvfzGnX6tvw2VTj0G9UyqrnWsGNRbAVwBE7Tshilq4ZEEdkS0labBfhJFhxRJSm0pINEcmX9KI9qB2ISbm8nVF00zoxxBl9M');
@@ -7,6 +8,7 @@ require_once('vendor/autoload.php');
 //Sanitize POST Array
 $POST= filter_var_array($_POST, FILTER_SANITIZE_STRING);
 
+$custId=$_SESSION['sessionId'];
 $email=$_SESSION['sessionEmail'];
 $fname=$_SESSION['sessionFname'];
 $lname=$_SESSION['sessionLname'];
@@ -31,7 +33,23 @@ $charge= \Stripe\Charge::create(array(
 
 ));
 
-//Redirect to success
-unset($_SESSION['totalAmt']);
-header('Location: orders.php?tid='.$charge->id.'&product='.$charge->description)
+print_r($customer);
+$currentTime= date("Y-m-d H:i:s");
+$sql = "insert into orders(orderTime, customerID) values(?,?)";
+        $stmt= mysqli_stmt_init($conn);
+        if(!mysqli_stmt_prepare($stmt, $sql)){
+            header("Location: payment.php?error=sqlerror0");
+            exit();
+
+        }else{
+            mysqli_stmt_bind_param($stmt, "ss", $currentTime,$custId);
+            mysqli_stmt_execute($stmt);
+            header("Location: orders.php?success=purchased");
+            exit();
+                
+            
+        }
+// //Redirect to success
+// unset($_SESSION['totalAmt']);
+// header('Location: orders.php?tid='.$charge->id.'&product='.$charge->description)
 ?>
