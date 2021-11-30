fcca22324f7f8d512eb807976d6fa14122e72ece
seyramfoli <lincolnfoli@gmail.com>
2021-11-30 15:58:45 +0000

    Update to code

--- signUp.php
+++ signUp.php
@@ -12,7 +12,7 @@
 </head>
 <body>
     <nav class="navbar navbar-expand-lg navbar-light bg-light justify-content-between ">
-    <a class="navbar-brand" href="#">Watch360</a>
+    <a class="navbar-brand" href="index.php">Watch360</a>
     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
     </button>
@@ -20,10 +20,10 @@
     <div class="collapse navbar-collapse" id="navbarSupportedContent">
         <ul class="navbar-nav mr-auto">
         <li class="nav-item active">
-            <a class="nav-link" href="#">Home <i class="fa fa-home" aria-hidden="true"></i><span class="sr-only">(current)</span></a>
+            <a class="nav-link" href="index.php">Home <i class="fa fa-home" aria-hidden="true"></i><span class="sr-only">(current)</span></a>
         </li>
         <li class="nav-item">
-            <a class="nav-link" href="#">Orders <i class="fa fa-clock-o" aria-hidden="true"></i></a>
+            <a class="nav-link" href="orders.php">Orders <i class="fa fa-clock-o" aria-hidden="true"></i></a>
         </li>
         </ul>
         <form class="form-inline my-2 my-lg-0">
